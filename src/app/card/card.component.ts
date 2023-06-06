f2471705ed6feeae822109510e9ed089ef4164c5
arleyCastaneda22 <arleycastaneda22@gmail.com>
2023-06-05 19:46:55 -0500

    Cambió

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  estado:boolean = true;
+  textoEstado:string ="Activo";
+  cambiarEstado():void{
+    this.estado = !this.estado;
 
+    if(this.estado){
+      this.textoEstado = "Activo";
+    }
+    else{
+      this.textoEstado = "Inactivo";
+    }
+  }
 }
