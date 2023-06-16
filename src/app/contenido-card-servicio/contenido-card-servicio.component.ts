e7e27df49376e83d40317c88910c4e5b583a770f
LaNoviaDeHeeseung <ac634600@gmail.com>
2023-06-16 11:23:58 -0500

    modales de vista servicios

--- src/app/contenido-card-servicio/contenido-card-servicio.component.ts
+++ src/app/contenido-card-servicio/contenido-card-servicio.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { DialogService} from '../service/dialog.service';
+import { DialogWithTemplateComponent } from '../components/dialog-with-template/dialog-with-template.component';
 
 @Component({
   selector: 'app-contenido-card-servicio',
@@ -6,6 +10,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./contenido-card-servicio.component.css']
 })
 export class ContenidoCardServicioComponent {
+
+  //editar boton
+
+  private matDialogRef !: MatDialogRef<DialogWithTemplateComponent>;
+
+  formGroup: FormGroup = new FormGroup({
+    name: new FormControl(),
+    lastname: new FormControl(),
+    Gmail: new FormControl(),
+    contraseña: new FormControl(),
+    ConfirmContraseña: new FormControl()
+  })
+
+
+  constructor(private dialogService:DialogService, ){}
+
+  openDialogCustom(){
+    this.dialogService.openDialogCustom({
+      title: ' ¿Estas seguro de eliminar este servicio?',
+      content: 'Al eliminar este servicio su informacion será eliminda permanentemente ¿Deseas Continuar?',
+    }).afterClosed().subscribe(res=> {
+      console.log('Dialog Custom Close', res)
+    this.formGroup.reset();
+    });
+  }
+
+  openDialogWithTemplate(template: TemplateRef<any>){
+    this.matDialogRef = this.dialogService.openDialogWithTemplate({
+      template,})
+      this.matDialogRef
+    .afterClosed().subscribe(res => {
+      console.log('Dialog with template Close', res)
+      this.formGroup.reset();
+    })
+  }
+
+  onSave(){
+    console.log(this.formGroup.value);
+    this.formGroup.reset()
+    this.matDialogRef.close()
+  }
+
+
+
+
+
   protected estado:boolean = true;
   protected textoEstado:string ="Activo";
 
